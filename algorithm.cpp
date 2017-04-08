44e8197400a324ef6b3d4d10692a84228ef931fb
Dhruv Somani <dsforweb@gmail.com>
2017-04-08 20:14:15 +0530

    Updated algorithm.cpp

--- algorithm.cpp
+++ algorithm.cpp
@@ -4,12 +4,14 @@
 #include <queue>
 #include <vector>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
 vector<string> solvePuzzle(string state);
 vector<string> reconstructPath(map<string, string> cameFrom, string current);
 vector<string> findNeighbors(string state);
+float calcfScore(string state);
 bool isSolved(string state, string finalState);
 
 int main() {
@@ -22,6 +24,7 @@ int main() {
     for (int index = 0; index < neighbors.size(); index++) cout << neighbors[index] << endl;
 
     vector<string> solution = solvePuzzle(state);
+    cout << "Solution: *****" << endl;
     for (int index = 0; index < solution.size(); index++) cout << solution[index] << endl;
 
     return 0;
@@ -36,18 +39,32 @@ vector<string> solvePuzzle(string state) {
 
     frontier.push_back(state);
 
+
     map<string, float> fScore;
     map<string, int> gScore;
     gScore[state] = 0;
 
     while (frontier.size() > 0) {
 
-        string curr_state = frontier.front();
-        frontier.erase(frontier.begin() + 1);
+        int best_choice = 0;
+        float minimum = 100;
 
-        cout << curr_state << endl;
-        for (int index = 0; index < visited.size(); index++) cout << visited[index];
+        for (int index = 0; index < frontier.size(); index++) {
+            if (fScore[frontier[index]] > minimum) best_choice = index;
+        }
+
+        string curr_state = frontier[best_choice];
+        frontier.erase(frontier.begin() + best_choice);
+        visited.push_back(curr_state);
+
+        // DEBUGGING
+        /*
+        cout << endl << curr_state << endl;// << "Visited: ";
+        //for (int index = 0; index < visited.size(); index++) cout << visited[index];
+        cout << endl << "Frontier: ";
+        for (int index = 0; index < frontier.size(); index++) cout << frontier[index];
         cout << endl;
+        */
 
         string final = "12345678 ";
 
@@ -56,25 +73,38 @@ vector<string> solvePuzzle(string state) {
             return path;
         }
 
-        visited.push_back(curr_state);
-        vector<string> neighbors = findNeighbors(state);
+
+        vector<string> neighbors = findNeighbors(curr_state);
 
         for (unsigned int index = 0; index < neighbors.size(); index++) {
+
             string neighbor = neighbors[index];
-            if (find(visited.begin(), visited.end(), neighbor) != visited.end()) continue;
-            if(gScore[neighbor] > gScore[curr_state] + 1) continue;
-
-            if (find(frontier.begin(), frontier.end(), neighbor) == frontier.end()) {
-                cout << "Yeah it did";
-                frontier.push_back(neighbor);
-
-                // This path is the best until now. Record it!
-                cameFrom[neighbor] = curr_state;
-                gScore[neighbor] = gScore[curr_state] + 1;
-
+            //cout << neighbor;
+            if (find(visited.begin(), visited.end(), neighbor) != visited.end()) {
+                    //cout << " terminated in visited." << endl;
+                    continue;
             }
 
+            if (find(frontier.begin(), frontier.end(), neighbor) == frontier.end()) {
+                //cout << " " << "State not yet Visited. State inserted in Frontier." << endl;
+                frontier.push_back(neighbor);
+
+            } else if ((gScore[curr_state] + 1) >= gScore[neighbor]) {
+                    //cout << " terminated due to idiotic reasons." << endl;
+                    continue;
+            }
+
+            // This path is the best until now. Record it!
+            cameFrom[neighbor] = curr_state;
+            fScore[neighbor] = calcfScore(neighbor);
+            gScore[neighbor] = gScore[curr_state] + 1;
+
         }
+
+
+        // mapping >>>>>>>>>>>>>>>>>>>>>>>>>>>
+        //for (std::map<string,float>::iterator it=fScore.begin(); it!=fScore.end(); ++it) {
+        //    std::cout << it->first << " => " << it->second << '\n';}
 
     }
 
@@ -98,6 +128,22 @@ vector<string> reconstructPath(map<string, string> cameFrom, string current) {
 }
 
 
+float calcfScore(string state) {
+
+    float fScore = 0;
+    string final = "12345678 ";
+
+    for (int index = 1; index < 9; index++) {
+        int loc = state.find(final[index - 1]);
+
+        fScore += abs(loc - (index - 1));
+
+    }
+
+    return fScore;
+
+}
+
 vector<string> findNeighbors(string state) {
 
     vector<string> neighbors;
@@ -117,14 +163,14 @@ vector<string> findNeighbors(string state) {
         neighbors.push_back(temp_state);
     }
 
-    if (index > 3) {
+    if (index > 2) {
         string temp_state = state;
         temp_state[index] = state[index - 3];
         temp_state[index - 3] = ' ';
         neighbors.push_back(temp_state);
     }
 
-    if (index < 7) {
+    if (index < 6) {
         string temp_state = state;
         temp_state[index] = state[index + 3];
         temp_state[index + 3] = ' ';
